76a595fd1aeba093f75ae0800da6cdb42470af91
tijoscarlos <tijoscarlos@gmail.com>
2016-03-28 19:10:43 +0200

    slides

--- app/common/carousel.php
+++ app/common/carousel.php
@@ -2,62 +2,71 @@
 			<div class="responisve-container">
 				<div class="slider">
 					<div class="fs_loader"></div>
+
 					<div class="slide">
-						<img 	src="scripts/slider/images/01_box_top.png"
-								width="361" height="300"			
-								data-position="-152,142" data-in="left" data-delay="200" data-out="right">
+					
+<!-- 						<img    src="static/images/logo/acosystlogo.png" width="auto" height="auto"  -->
+<!-- 								data-position="240,200" data-in="top" data-step="2" data-easeIn="easeOutCirc"> -->
 								
-						<img    src="scripts/slider/images/01_box_bottom.png" 
-								width="422" height="300" 		
-								data-position="138,-152" data-in="bottomRight" data-delay="200">
+						<img 	src="static/images/logo/acosystlogo.png" 
 								
-						<img    src="scripts/slider/images/01_waves.png" 
-								width="1449" height="115"
-								data-position="240,17" data-in="left" data-delay="" data-out="left">
+								data-position="15,445" data-in="top" data-delay="0" data-out="right">
 						
-						<op 		class="claim light-green"			
-								data-position="30,30" data-in="top" data-step="1" data-out="top" data-ease-in="easeOutBounce">jQuery Fraction Slider</op>
-														
-						<op 		class="teaser orange" 	
-								data-position="90,30" data-in="left" data-step="2" data-delay="500">animate multiple elements</op>		
-						<op 		class="teaser green" 	
-								data-position="90,30" data-in="left" data-step="2" data-special="cycle" data-delay="3000">full control over each element</op>		
-						<op 		class="teaser turky"		
-								data-position="90,30" data-in="left" data-step="2" data-special="cycle" data-delay="5500" data-out="none">opensource and free</op>
+						<op 	class="claim" 			
+								data-position="30,30" data-in="top"  data-delay="500" data-out="top">NOS METIERS</op>
+									
+	  				
+						<img 	src="static/images/autres/creation-site-web.png" 
+								data-position="105,500" data-in="left" data-step="2" data-delay="1000" data-out="bottomRight">
+						<op 	class="teaser turky small" 	
+								data-position="100,30" data-in="bottom" data-step="2" data-delay="1000">Cr&eacute;ation des sites et Applications Web</op>		
+		
 						
-						<img 	src="scripts/slider/images/01_outofthebox.png" 
-								data-position="20,330" data-in="bottomLeft" data-delay="500" data-out="fade">
+						<img 	src="static/images/autres/formation.png" 	
+								data-position="80,220" data-in="left" data-step="3" data-delay="1000" data-out="bottomRight">
+						<op 	class="teaser turky small" 	
+								data-position="150,30" data-in="bottom" data-step="3" data-delay="1000">Formation en nouvelles Technologies</op>
+	
+						<img    src="static/images/autres/service.png" 
+								data-position="138,-152" data-in="left" data-step="4" data-delay="1000" data-out="bottomRight">
+						<op 	class="teaser turky small" 	
+								data-position="200,30" data-in="bottom" data-step="4" data-delay="1000">Conseil en Technologie de l'Information</op>	
+								
 					</div>
+					
 					<div class="slide">
-						<img 	src="scripts/slider/images/02_big_boxes.png" 		
-								data-position="25,445" data-in="fade" data-delay="0" data-out="right">
+					
+<!-- 						<img    src="static/images/logo/acosystlogo.png" width="auto" height="auto"  -->
+<!-- 								data-position="240,200" data-in="top" data-step="2" data-easeIn="easeOutCirc"> -->
 								
-						<img 	src="scripts/slider/images/02_small_boxes.png" 	
+						
+						
+						<img 	src="static/images/logo/acosystlogo.png" 
+								
+								data-position="15,495" data-in="fade" data-delay="0" data-out="right">
+						<op 	class="claim" 			
+								data-position="30,0" data-in="top" ata-delay="500" data-out="top">NOS SECTEURS D'ACTIVITE</op>
+								
+						<img 	src="static/images/autres/formation.png" 	
 								data-position="80,220" data-in="fade" data-delay="500" data-out="bottomRight">
 						
-						<img    src="scripts/slider/images/01_box_bottom.png" 
+						<img    src="static/images/autres/service.png" 
 								data-position="138,-152" data-in="bottomRight" data-delay="200" data-out="bottomRight">
 						
-						<op 		class="claim light-green" 			
-								data-position="30,30" data-in="top" data-step="1" data-out="top">NOS METIERS</op>
+						
 										
+						<op 	class="teaser turky small" 	
+								data-position="100,30" data-in="bottom" data-step="2" data-delay="500">Education</op>		
 						<op 		class="teaser turky small" 	
-								data-position="90,30" data-in="bottom" data-step="2" data-delay="500">Cr&eacute;ation des sites et Applications Web</op>		
+								data-position="150,30" data-in="bottom" data-step="2" data-delay="1500">Services Publique & Priv&eacute;</op>
 						<op 		class="teaser turky small" 	
-								data-position="120,30" data-in="bottom" data-step="2" data-delay="1500">Formation en nouvelles Technologies</op>
-						<op 		class="teaser turky small" 	
-								data-position="150,30" data-in="bottom" data-step="2" data-delay="2500">Conseil en Technologie de l'Information</op>	
-						<op 		class="teaser turky small" 	
-								data-position="180,30" data-in="bottom" data-step="2" data-delay="3500">background animation</op>
-						<op 		class="teaser turky small" 	
-								data-position="210,30" data-in="bottom" data-step="2" data-delay="4500">easy to use</op>
+								data-position="200,30" data-in="bottom" data-step="2" data-delay="2500">Banques et assurances</op>	
+						
 								
-						<img    src="scripts/slider/images/01_waves.png" 
-								width="1449" height="115"
-								data-position="240,200" data-in="right" data-step="2" data-easeIn="easeOutCirc">
+						
 								
-						<img 	src="scripts/slider/images/02_main.png" 
-								data-position="105,180" data-in="fade" data-delay="500" data-out="bottomRight">
+						<img 	src="static/images/autres/banque-assurance.png" 
+								data-position="105,500" data-in="fade" data-delay="500" data-out="bottomRight">
 					</div>
 				</div>
 			</div>
